3d7c2427f053bc248a24fe3dd636a501da7eb603
Aldi <Aldi.Rahman@sera.astra.co.id>
2022-11-14 00:31:33 +0700

    Update

---
+++ tempData.js
@@ -0,0 +1,43 @@
+export default tempData = [
+  {
+    id: 1,
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Entertainment",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Watch Movie",
+        completed: false
+      },
+      {
+        title: "Playing football",
+        completed: false
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "School",
+    color: "#595BD9",
+    todos: [
+      {
+        title: "Forum",
+        completed: true
+      },
+      {
+        title: "Quiz",
+        completed: false
+      }
+    ]
+  }
+]
